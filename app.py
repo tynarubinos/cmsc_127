8dfcb286d7935ca8cdab5e6bc5404fee1fe11d74
Tyna <tyna.rubinos@module-zero.co>
2023-06-27 00:54:03 +0800

    frontend stuff

--- app.py
+++ app.py
@@ -1,87 +1,139 @@
 from flask import Flask, render_template, request, redirect
 import mysql.connector
 
+
 app = Flask(__name__)
 
 # Configure MySQL connection
 db = mysql.connector.connect(
-    host="localhost",
+    host="127.0.0.1",
     user="root",
-    database="dbsorya_db"
+    database="dbsorya_db",
+    password="123456"
 )
 
 # Create a cursor to interact with the database
 cursor = db.cursor()
 
-# Define the data model
-class InventoryItem:
-    def __init__(self, id, name, quantity, price):
-        self.id = id
-        self.name = name
-        self.quantity = quantity
-        self.price = price
-
-# Home page to display all inventory items
+# Homepage 
 @app.route("/")
 def home():
-    # Fetch all items from the database
-    cursor.execute("SELECT * FROM inventory")
+    # Render home
+    return render_template("home.html")
+
+######################
+# Products
+######################
+
+# Define the data model
+class ProductItem:
+    def __init__(self, 
+                 product_code, 
+                 category_id, 
+                 name, 
+                 date_acquired, 
+                 expiration_date, 
+                 quantity, 
+                 cost, 
+                 storage_location, 
+                 supplier_id):
+        self.product_code = product_code
+        self.category_id = category_id
+        self.name = name
+        self.date_acquired = date_acquired
+        self.expiration_date = expiration_date
+        self.quantity = quantity
+        self.cost = cost
+        self.storage_location = storage_location
+        self.supplier_id = supplier_id
+        
+# Fetch all products from the database
+@app.route("/product")
+def product():
+    cursor.execute("SELECT * FROM product")
     rows = cursor.fetchall()
 
-    # Create InventoryItem objects for each row
-    inventory_items = []
+    # Create Product objects for each row
+    product_list = []
     for row in rows:
-        item = InventoryItem(row[0], row[1], row[2], row[3])
-        inventory_items.append(item)
+        item = ProductItem(row[0], row[1], row[2], row[3],
+                       row[4], row[5], row[6], row[7],
+                       row[8])
+        product_list.append(item)
 
-    # Render the template with the inventory data
-    return render_template("home.html", inventory=inventory_items)
+    # Render template with inventory data
+    return render_template("product.html", product=product_list)
 
-# Add new item to the inventory
-@app.route("/add", methods=["GET", "POST"])
-def add_item():
+# Add new product to the inventory
+@app.route("/add_product", methods=["GET", "POST"])
+def add_product():
     if request.method == "POST":
+        product_code = request.form["product_code"]
+        category_id = request.form["category_id"]
         name = request.form["name"]
+        date_acquired = request.form["date_acquired"]
+        expiration_date = request.form["expiration_date"]
         quantity = int(request.form["quantity"])
-        price = float(request.form["price"])
+        cost = float(request.form["cost"])
+        storage_location = request.form["storage_location"]
+        supplier_id = request.form["supplier_id"]
 
         # Insert the new item into the database
-        cursor.execute("INSERT INTO inventory (name, quantity, price) VALUES (%s, %s, %s)", (name, quantity, price))
+        cursor.execute("INSERT INTO product (product_code, category_id, name, date_acquired, expiration_date, quantity, cost, storage_location, supplier_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", 
+                       (product_code, category_id, name, date_acquired, 
+                        expiration_date, quantity, cost, storage_location, supplier_id))
         db.commit()
 
-        return redirect("/")
+        return redirect("/product")
     else:
-        return render_template("add.html")
+        return render_template("add_product.html")
+#################### OK
 
-# Edit an existing item in the inventory
-@app.route("/edit/<int:item_id>", methods=["GET", "POST"])
-def edit_item(item_id):
+# Edit an existing product item in the table
+@app.route("/edit_product/<string:product_code>", methods=["GET", "POST"])
+def edit_product(product_code):
     if request.method == "POST":
+        category_id = request.form["category_id"]
         name = request.form["name"]
+        date_acquired = request.form["date_acquired"]
+        expiration_date = request.form["expiration_date"]
         quantity = int(request.form["quantity"])
-        price = float(request.form["price"])
+        cost = float(request.form["cost"])
+        storage_location = request.form["storage_location"]
+        supplier_id = request.form["supplier_id"]
 
         # Update the item in the database
-        cursor.execute("UPDATE inventory SET name=%s, quantity=%s, price=%s WHERE id=%s", (name, quantity, price, item_id))
+        cursor.execute("UPDATE product SET category_id=%s, name=%s, date_acquired=%s, expiration_date=%s, quantity=%s, cost=%s, storage_location=%s, supplier_id=%s WHERE product_code=%s", 
+                       (category_id, name, date_acquired, expiration_date, quantity, cost, storage_location, supplier_id, product_code))
         db.commit()
 
-        return redirect("/")
+        return redirect("/product")
     else:
-        # Fetch the item from the database
-        cursor.execute("SELECT * FROM inventory WHERE id = %s", (item_id,))
+        cursor.execute("SELECT * FROM product WHERE product_code = %s", (product_code,))
         row = cursor.fetchone()
-        item = InventoryItem(row[0], row[1], row[2], row[3])
+        item = ProductItem(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8])
 
-        return render_template("edit.html", item=item)
+        return render_template("edit_product.html", item=item)
+############ OK
 
-# Delete an item from the inventory
-@app.route("/delete/<int:item_id>")
-def delete_item(item_id):
-    # Delete the item from the database
-    cursor.execute("DELETE FROM inventory WHERE id = %s", (item_id,))
+# Delete an item from the product table
+@app.route("/delete_product/<string:product_code>")
+def delete_product(product_code):
+    # Delete product from the database
+    cursor.execute("DELETE FROM product WHERE product_code = %s", (product_code,))
     db.commit()
 
-    return redirect("/")
+    return redirect("/product")
+############ OK
+
+######################
+# Suppliers
+######################
+
+######################
+# Categories
+######################
+
 
 if __name__ == "__main__":
     app.run(debug=True)
