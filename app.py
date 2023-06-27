c757fabf7d5629db65c019abc4292c5045020045
Tyna <tyna.rubinos@module-zero.co>
2023-06-27 12:31:16 +0800

    supplier, category OK

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import mysql.connector
 
-
 app = Flask(__name__)
 
 # Configure MySQL connection
@@ -36,7 +35,7 @@ class ProductItem:
                  quantity, 
                  cost, 
                  storage_location, 
-                 supplier_id):
+                 id):
         self.product_code = product_code
         self.category_id = category_id
         self.name = name
@@ -45,7 +44,7 @@ class ProductItem:
         self.quantity = quantity
         self.cost = cost
         self.storage_location = storage_location
-        self.supplier_id = supplier_id
+        self.id = id
         
 # Fetch all products from the database
 @app.route("/product")
@@ -76,12 +75,12 @@ def add_product():
         quantity = int(request.form["quantity"])
         cost = float(request.form["cost"])
         storage_location = request.form["storage_location"]
-        supplier_id = request.form["supplier_id"]
+        id = request.form["id"]
 
         # Insert the new item into the database
-        cursor.execute("INSERT INTO product (product_code, category_id, name, date_acquired, expiration_date, quantity, cost, storage_location, supplier_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", 
+        cursor.execute("INSERT INTO product (product_code, category_id, name, date_acquired, expiration_date, quantity, cost, storage_location, id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", 
                        (product_code, category_id, name, date_acquired, 
-                        expiration_date, quantity, cost, storage_location, supplier_id))
+                        expiration_date, quantity, cost, storage_location, id))
         db.commit()
 
         return redirect("/product")
@@ -101,11 +100,11 @@ def edit_product(product_code):
         quantity = int(request.form["quantity"])
         cost = float(request.form["cost"])
         storage_location = request.form["storage_location"]
-        supplier_id = request.form["supplier_id"]
+        id = request.form["id"]
 
         # Update the item in the database
-        cursor.execute("UPDATE product SET product_code=%s, category_id=%s, name=%s, date_acquired=%s, expiration_date=%s, quantity=%s, cost=%s, storage_location=%s, supplier_id=%s WHERE product_code=%s", 
-                       (product_code, category_id, name, date_acquired, expiration_date, quantity, cost, storage_location, supplier_id, product_code))
+        cursor.execute("UPDATE product SET product_code=%s, category_id=%s, name=%s, date_acquired=%s, expiration_date=%s, quantity=%s, cost=%s, storage_location=%s, id=%s WHERE product_code=%s", 
+                       (product_code, category_id, name, date_acquired, expiration_date, quantity, cost, storage_location, id, product_code))
         db.commit()
 
         return redirect("/product")
@@ -130,11 +129,156 @@ def delete_product(product_code):
 ######################
 # Suppliers
 ######################
+class Supplier:
+    def __init__(self, 
+                 id, 
+                 name,
+                 address,
+                 contact_number):
+        self.id = id
+        self.name = name
+        self.address = address
+        self.contact_number = contact_number
+        
+# Fetch all suppliers from the database
+@app.route("/supplier")
+def supplier():
+    cursor.execute("SELECT * FROM supplier")
+    rows = cursor.fetchall()
+
+    # Create Supplier item for each row
+    supplier_list = []
+    for row in rows:
+        item = Supplier(row[0], row[1], row[2], row[3])
+        supplier_list.append(item)
+
+    # Render template with inventory data
+    return render_template("supplier.html", supplier=supplier_list)
+########## OK
+
+# Add new suppliers
+@app.route("/add_supplier", methods=["GET", "POST"])
+def add_supplier():
+    if request.method == "POST":
+        id = request.form["id"]
+        name = request.form["name"]
+        address = request.form["address"]
+        contact_number = request.form["contact_number"]
+
+        # Insert the new supplier into the database
+        cursor.execute("INSERT INTO supplier (id, name, address, contact_number) VALUES (%s, %s, %s, %s)", 
+                       (id, name, address, contact_number))
+        db.commit()
+
+        return redirect("/supplier")
+    else:
+        return render_template("add_supplier.html")
+#################### OK
+
+# Edit suppliers
+@app.route("/edit_supplier/<int:id>", methods=["GET", "POST"])
+def edit_supplier(id):
+    if request.method == "POST":
+        updated_id = request.form["id"]
+        name = request.form["name"]
+        address = request.form["address"]
+        contact_number = request.form["contact_number"]
+
+        # Update the supplier in the database
+        cursor.execute("UPDATE supplier SET id=%s, name=%s, address=%s, contact_number=%s WHERE id=%s", 
+                       (id, name, address, contact_number, id))
+        db.commit()
+
+        return redirect("/supplier")
+    else:
+        cursor.execute("SELECT * FROM supplier WHERE id = %s", (id,))
+        row = cursor.fetchone()
+        item = Supplier(row[0], row[1], row[2], row[3])
+
+        return render_template("edit_supplier.html", item=item)
+######### OK     
+
+# Delete a supplier from the supplier table
+@app.route("/delete_supplier/<int:id>")
+def delete_supplier(id):
+    # Delete supplier from the database
+    cursor.execute("DELETE FROM supplier WHERE id = %s", (id,))
+    db.commit()
+
+    return redirect("/supplier")
+######### OK
 
 ######################
 # Categories
 ######################
+class Category:
+    def __init__(self, 
+                 id, 
+                 name):
+        self.id = id
+        self.name = name
+        
+# Fetch all categories from the database
+@app.route("/category")
+def category():
+    cursor.execute("SELECT * FROM category")
+    rows = cursor.fetchall()
 
+    # Create Category item for each row
+    category_list = []
+    for row in rows:
+        item = Category(row[0], row[1])
+        category_list.append(item)
+
+    # Render template with inventory data
+    return render_template("category.html", category=category_list)
+
+# Add new category
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        id = int(request.form["id"])
+        name = request.form["name"]
+
+        # Insert the new category into the database
+        cursor.execute("INSERT INTO category (id, name) VALUES (%s, %s)", (id, name,))
+        db.commit()
+
+        return redirect("/category")
+    else:
+        return render_template("add_category.html")
+
+#################### OK
+
+# Edit category
+@app.route("/edit_category/<int:id>", methods=["GET", "POST"])
+def edit_category(id):
+    if request.method == "POST":
+        updated_id = request.form["id"]
+        name = request.form["name"]
+
+        # Update the category in the database
+        cursor.execute("UPDATE category SET id=%s, name=%s WHERE id=%s",
+                       (updated_id, name, id))
+        db.commit()
+
+        return redirect("/category")
+    else:
+        cursor.execute("SELECT * FROM category WHERE id = %s", (id,))
+        row = cursor.fetchone()
+        item = Category(row[0], row[1])
+
+        return render_template("edit_category.html", item=item)
+    
+# Delete a category from the category table
+@app.route("/delete_category/<int:id>")
+def delete_category(id):
+    # Delete the category from the database
+    cursor.execute("DELETE FROM category WHERE id = %s", (id,))
+    db.commit()
+
+    return redirect("/category")
+######### OK
 
 if __name__ == "__main__":
     app.run(debug=True)
